22d907d9d68271f04e8fcab8331c8f0dc8021873
Alexandre Cardoso <xandecar@hotmail.com>
2020-03-30 13:23:56 -0300

    Base Model agora tem metodos default para todos Models.

--- src/database/models/Base.php
+++ src/database/models/Base.php
@@ -2,6 +2,7 @@
 
 namespace src\database\models;
 
+use PDOException;
 use src\database\Connection;
 
 abstract class Base
@@ -11,5 +12,17 @@ abstract class Base
     public function __construct()
     {
         $this->pdoInstance = Connection::connectPDO();
+    }
+
+    public function findUserBy($field, $value)
+    {
+        try {
+            $preparedSql = $this->pdoInstance->prepare("select * from {$this->table} where {$field} = :{$field}");
+            $preparedSql->bindValue(":{$field}", $value);
+            $preparedSql->execute();
+            return $preparedSql->fetch();
+        } catch (PDOException $e) {
+            var_dump($e->getMessage());
+        }
     }
 }
--- src/database/models/User.php
+++ src/database/models/User.php
@@ -6,15 +6,5 @@ use PDOException;
 
 class User extends Base
 {
-    public function findUserBy($field, $value)
-    {
-        try {
-            $preparedSql = $this->pdoInstance->prepare("select * from users where {$field} = :{$field}");
-            $preparedSql->bindValue(":{$field}", $value);
-            $preparedSql->execute();
-            return $preparedSql->fetch();
-        } catch (PDOException $e) {
-            var_dump($e->getMessage());
-        }
-    }
+    protected $table = 'users';
 }
